3aefcf02f879449ff58fe5ea14c03158b5a56eff
Jigyasu Prakash <jigyasuprakash3@gmail.com>
2020-05-11 11:49:58 +0530

    weather fetch

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,9 +14,22 @@ nunjucks.configure('views', {
 app.post('/webhook', (req, res) => {
     if (!req.body) return res.sendStatus(400);
     res.setHeader('Content-Type', 'application/json');
-    console.log(req.body);
     var city = req.body.queryResult.parameters['geo-city'];
     console.log("Queried city is: " + city);
+
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=59f1493446889d1ab3cc857582a0bda7`)
+        .then((response) => {
+            const result = `Right now in ${city} it feels ${response.data.weather[0].description} with a temperature of ${response.data.main.temp - 273.15} degree Celsius.`
+            const resObj = {
+                fulfillmentText: " ",
+                fulfillmentMessages: [{ "text": { "text": [result] } }],
+                source: "OpenWeather"
+            }
+            return res.json(resObj);
+        })
+        .catch((err) => {
+            console.error("Somthing went wrong", err);
+        })
 })
 
 app.get('/', (req, res) => {
